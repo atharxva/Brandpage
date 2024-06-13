9999de43a091a8982bc68e033b8504cbe1766cdf
atharxva <2023.atharvas@isu.ac.in>
2024-06-13 15:33:30 +0530

    First commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+function Hero() {
+    return (
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>
+                    YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP YOU WITH
+                    OUR SHOES.YOUR FEET DESERVE THE BEST AND WERE HERE TO HELP
+                    YOU WITH OUR SHOES.
+                </p>
+                <div className="hero-button">
+                    <button>Shop Now</button>
+                    <button>Category</button>
+                </div>
+                <div className="Shopping">
+                    <p>Also available on</p>
+                    <div className="brand-icons">
+                        <img src></img>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero-image"></div>
+        </main>
+    );
+}
+
+export default Hero;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+function Navigation() {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="src/assets/images/brand_logo.png" alt="logo"></img>
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    );
+}
+
+export default Navigation;
